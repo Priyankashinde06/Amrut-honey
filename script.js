fa2b23655574adda66f70dde6c4a35b5adfe97dd
Priyankashinde06 <ps662001@gmail.com>
2025-06-02 09:50:16 +0530

    Add new SVG images and update HTML/CSS for honey slider
    - Added new SVG files: honey-2.svg and honey-3.svg for visual elements.
    - Introduced multiple new JPG images for the slider in the slider directory.
    - Updated index.html to fix a class name typo in the hero section.
    - Minified and updated swiper-bundle.js for improved performance.
    - Created new HTML file (new.html) for a honeycomb slider with responsive design.
    - Enhanced script.js to include a new Swiper instance for book table image slider.
    - Refactored style.css to improve layout and styling for the organic honey choose section and book table image slider.

--- script.js
+++ script.js
@@ -1279,3 +1279,30 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+var book_table = new Swiper(".book-table-img-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+    },
+    speed: 2000,
+    effect: "coverflow",
+    coverflowEffect: {
+        rotate: 3,
+        stretch: 2,
+        depth: 100,
+        modifier: 5,
+        slideShadows: false
+    },
+    loopAdditionSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    }
+});
